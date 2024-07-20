1404640e335d311261d48cd71ea309a834ba3346
David Ayala-Irizarry <sgprboricua1>
2024-07-20 07:37:43 -0400

    got POST and GET working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 const Task = require('../../models/Task');
 
+router.get('/', async(req, res) => {
+    const allTasks = await Task.findAll()
+    res.json(allTasks);
+})
+
 // localhost:3001/api
-router.post('/', (req ,res) => {
-    const newTask = Task.create ({
-        name:'San German, Puerto Rico',
-        description: 'My pueblo donde creci',
-        status: 'todo'
+router.post('/', async (req ,res) => {
+    const newTask = await Task.create ({
+        // const {name, description, status } = req.body; object deconstructuring mode
+        name: req.body.name,
+        description: req.body.description,
+        status: req.body.status
     });
 
     res.json(newTask);
